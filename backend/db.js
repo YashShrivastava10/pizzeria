fb0ca1d58d5e803003db992115e63b0e8466ff4a
YashShrivastava10 <shrivastavayash10@gmail.com>
2023-10-14 17:41:36 +0530

    Server and db connect

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const  { MongoClient, ServerApiVersion } = require("mongodb");
+const url = "mongodb+srv://shrivastavayash10:simplepassword@cluster1.zxsuzyp.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+const dbName = 'pizzeria';
+
+async function connectToDB() {
+  await client.connect();
+  console.log('Connected successfully to server');
+  return client.db(dbName);
+}
+
+module.exports = connectToDB
--- backend/pizzeriaBE.js
+++ backend/pizzeriaBE.js
@@ -1,9 +1,13 @@
+const connectDB = require("./db")
 const server = require("express")()
 const port = 4000
 
-
-server.get("/pizza", (req, res) => {
-  res.send("Hello")
+server.get("/pizza", async(req, res) => {
+  let data = await connectDB()
+  const collection = await data.collection("pizza")
+  data = await collection.find().toArray()
+  console.log(data);
+  res.send(data)
 })
 
 server.listen(port, () => console.log(`Server running at port ${port}`))
