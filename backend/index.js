210fc66772db0aa1cf9348c971f994b837164772
YashShrivastava10 <shrivastavayash10@gmail.com>
2023-10-15 16:50:29 +0530

    Debugging api route 5

--- backend/index.js
+++ backend/index.js
@@ -4,30 +4,27 @@ import express from "express"
 import cors from "cors"
 const server = express()
 
-server.use(cors({
-  "origin": ["http://localhost:3000"],
-  "credentials": true
-}))
+server.use(cors())
 
 const port = 4000
 
 const collectionData = async(name) => {
   const data = await connectDB()
-  const collection = await data.collection(name)
+  const collection = data.collection(name)
   return await collection.find().toArray()
 }
 
 server.get("/", (req, res) => res.send("Hello"))
+
 server.get("/pizza", async (req, res) => {
-  res.send("Pizza Details")
-  // try {
-  //   const data = await collectionData("pizza");
-  //   console.log("Pizza data:", data); // Add this line for debugging
-  //   res.send(data);
-  // } catch (error) {
-  //   console.error("Error fetching pizza data:", error); // Add this line for debugging
-  //   res.status(500).send("Internal Server Error");
-  // }
+  try {
+    const data = await collectionData("pizza");
+    console.log("Pizza data:", data); // Add this line for debugging
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching pizza data:", error); // Add this line for debugging
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 server.get("/users", async (req, res) => {
