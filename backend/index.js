4dca1391e32c5927a6e64cf0903179d2dc26970d
YashShrivastava10 <shrivastavayash10@gmail.com>
2023-10-15 16:21:51 +0530

    Debugging api route

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ import express from "express"
 import cors from "cors"
 const server = express()
 
-server.use(cors())
+server.use(cors({
+  "origin": ["http://localhost:3000"],
+  "credentials": true
+}))
+
 const port = 4000
 
 const collectionData = async(name) => {
@@ -14,14 +18,26 @@ const collectionData = async(name) => {
 }
 
 server.get("/", (req, res) => res.send("Hello"))
-server.get("/pizza", async(req, res) => {
-  const data = await collectionData("pizza")
-  res.send(data)
-})
+server.get("/pizza", async (req, res) => {
+  try {
+    const data = await collectionData("pizza");
+    console.log("Pizza data:", data); // Add this line for debugging
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching pizza data:", error); // Add this line for debugging
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-server.get("/users", async(req, res) => {
-  const data = await collectionData("users")
-  res.send(data)
+server.get("/users", async (req, res) => {
+  try {
+    const data = await collectionData("users");
+    console.log("Users data:", data); // Add this line for debugging
+    res.send(data);
+  } catch (error) {
+    console.error("Error fetching users data:", error); // Add this line for debugging
+    res.status(500).send("Internal Server Error");
+  }
 })
 
 server.listen(port, () => console.log(`Server running at port ${port}`))
