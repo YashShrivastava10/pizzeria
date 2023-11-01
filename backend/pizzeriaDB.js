0454375e2414874ca1d89955db6a8e0b369d53b9
YashShrivastava10 <shrivastavayash10@gmail.com>
2023-11-01 14:15:48 +0530

    Password encrypt & decrypt

--- backend/pizzeriaDB.js
+++ backend/pizzeriaDB.js
@@ -1,12 +1,37 @@
-
 import connectDB from "./db.js"
 import express from "express"
 import cors from "cors"
+import bcrypt from "bcrypt"
+
 const server = express()
 
 server.use(cors())
 
 const port = 4000
+const saltRounds = 10
+
+const encrypt = async(pass) => {
+  try{
+    const hash = await bcrypt.hash(pass, saltRounds)
+    return hash
+  }
+  catch(error){
+    console.log("Can't encrypt");
+    throw error
+  }
+}
+
+const verifyPassword = async(typedPassword, userPassword) => {
+  try{
+    const isMatch = await bcrypt.compare(typedPassword, userPassword) // First argument should be non-hashed
+    if(isMatch) return true
+    return false
+  }
+  catch(error){
+    console.log("Can't decrypt");
+    throw error
+  }
+}
 
 const collectionData = async (name) => {
   const data = await connectDB()
@@ -19,7 +44,7 @@ server.get("/pizza", async (req, res) => {
     const data = await collectionData("pizza");
     res.send(data);
   } catch (error) {
-    console.error("Error fetching pizza data:", error); // Add this line for debugging
+    console.error("Error fetching pizza data:", error);
     res.status(500).send("Internal Server Error");
   }
 });
@@ -29,7 +54,7 @@ server.get("/ingredients", async (req, res) => {
     const data = await collectionData("ingredients");
     res.send(data);
   } catch (error) {
-    console.error("Error fetching ingredients data:", error); // Add this line for debugging
+    console.error("Error fetching ingredients data:", error);
     res.status(500).send("Internal Server Error");
   }
 });
@@ -38,18 +63,22 @@ server.post("/signUp", async (req, res) => {
   try {
     let data = await connectDB()
     const collection = data.collection("users")
+
     const { name, email, pass } = req.query
+
     data = await collection.find({ email: email }).toArray()
+
     if (data.length === 0) {
-      const result = await collection.insertOne({ name, email, password: pass })
-      console.log(result);
-      if (result.acknowledged) return res.status(200).send({ success: true, message: "Sign In Successfull" })
+      const hash = await encrypt(pass)
+      const result = await collection.insertOne({ name, email, password: hash })
+
+      if(result.acknowledged) return res.status(200).send({ success: true, message: "Sign In Successfull" })
       else return res.status(200).send({ success: false, message: "Sign In Failed" })
     }
     else return res.status(200).send({ success: false, message: "Email Id already Exist" })
   }
   catch(error){
-    console.error("Error sigin:", error); // Add this line for debugging
+    console.error("Error sigin:", error);
     res.status(500).send({ message: error });
   }
 })
@@ -58,24 +87,20 @@ server.post("/login", async (req, res) => {
   try {
     let data = await connectDB()
     const collection = data.collection("users")
-    const { email, pass } = req.query
-    data = await collection.find({ email: email }).toArray()
-    if (data.length === 0) return res.status(200).send({ success: false, message: "Email Id doest not exist", data: {} })
-    if (data[0].password === pass) return res.status(200).send({ success: true, message: "Login Successfull", data: data[0] })
-    return res.status(200).send({ success: false, message: "Incorrect Password", data: {} })
-  } catch (error) {
-    console.error("Error login:", error); // Add this line for debugging
-    res.status(500).send({ message: error });
-  }
-})
 
-server.get("/users", async (req, res) => {
-  try {
-    const data = await collectionData("users");
-    res.send(data);
-  } catch (error) {
-    console.error("Error fetching users data:", error); // Add this line for debugging
-    res.status(500).send("Internal Server Error");
+    const { email, pass } = req.query
+
+    data = await collection.findOne({ email: email })
+
+    if (!data) return res.status(200).send({ success: false, message: "Email Id doest not exist", data: {} })
+
+    if (await verifyPassword(pass, data.password)) return res.status(200).send({ success: true, message: "Login Successfull", data: (({ password, ...data }) => data)(data) })
+
+    return res.status(200).send({ success: false, message: "Incorrect Password", data: {} })
+  }
+  catch (error) {
+    console.error("Error login:", error);
+    res.status(500).send({ message: error });
   }
 })
 
