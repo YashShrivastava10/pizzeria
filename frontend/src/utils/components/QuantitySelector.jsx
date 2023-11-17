b99b9a03fd7734e8849e43e28cf85830675eec09
YashShrivastava10 <shrivastavayash10@gmail.com>
2023-11-17 16:51:50 +0530

    Border issue in Quantity selector

--- frontend/src/utils/components/QuantitySelector.jsx
+++ frontend/src/utils/components/QuantitySelector.jsx
@@ -4,9 +4,9 @@ import { increment, decrement } from "../cartUtil"
 export const QuantitySelector = ({data, dispatch, quant}) => {
   return (
     <>
-      <button className="rounded-l-full qty-btn" onClick={() => decrement(data.id, quant ? quant - 1 : data.qty - 1, dispatch)}>-</button>
+      <button className="left-btn qty-btn" onClick={() => decrement(data.id, quant ? quant - 1 : data.qty - 1, dispatch)}>-</button>
       <span className="border-t border-b border w-full text-center">{quant ? quant : data.qty}</span>
-      <button className="rounded-r-full qty-btn" onClick={() => increment(data.id, quant ? quant + 1 : data.qty + 1, dispatch)}>+</button>
+      <button className="right-btn qty-btn" onClick={() => increment(data.id, quant ? quant + 1 : data.qty + 1, dispatch)}>+</button>
     </>
   )
 }
